d3ef7d6b7573bad8d869f24d137c0ca41d66089a
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-11-08 20:17:31 -0600

    add bools, expanded

---
+++ 4_booleans.py
@@ -0,0 +1,30 @@
+"""
+Booleans are a data type that can hold one of two values: True or False. 
+They are often used in conditional statements to control the flow of a program. 
+"""
+
+# You can store a boolean in a variable like this:
+my_boolean = True
+
+# You can also store the result of a comparison in a variable:
+a = 10
+b = 5
+result = a > b
+print(result)
+# What is the value of result?
+
+
+# Can you print the value of if a = b?
+
+# Can you print the value of if a > b?
+
+# How do you check if a is not equal to b?
+
+
+# Can we do this for strings? 
+# Does "hi" equal "hi"?
+
+# Does "Hello" equal "hello"?
+
+
+
